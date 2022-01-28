8efaa1dedc7c022fb112db572841aebc1fa00fa5
BRQ <bnowak4744@gmail.com>
2022-01-28 17:58:59 +0100

    Added solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
 
+    if(repeats === 0)
+        return "";
+    else if(repeats < 0)
+        return "ERROR";
+    return string.repeat(repeats);
 };
+
+let a = 3;
 
 // Do not edit below this line
 module.exports = repeatString;
