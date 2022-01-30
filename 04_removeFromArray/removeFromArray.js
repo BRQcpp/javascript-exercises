c251aee5126f53a4024ca1a78e4ff52d60ea14cb
BRQ <bnowak4744@gmail.com>
2022-01-30 15:07:50 +0100

    Added solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
 
+    array = array.filter( (array) => {
+        for(let j = 0; j < values.length; j++)
+                if(array === values[j])
+                    return false;
+            return true;
+        });
+   
+    return array;
 };
+
+let array = removeFromArray([1,2,3,4], 2);
+
+console.log(array);
 
 // Do not edit below this line
 module.exports = removeFromArray;
