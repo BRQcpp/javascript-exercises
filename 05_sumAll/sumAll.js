fb9261f467f89cec9204a3fde8d20b0893093a2e
BRQ <bnowak4744@gmail.com>
2022-01-30 15:48:14 +0100

    Added solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
 
+    if(first < 0 || second < 0 || isNaN(first) || isNaN(second) || typeof first === typeof"" || typeof second === typeof"")
+        return "ERROR";
+
+    if(first > second)
+    {
+        let temp;
+        temp = first;
+        first = second;
+        second = temp;
+    }
+
+    return ((first + second)/2)* second;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
