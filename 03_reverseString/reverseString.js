5dc47bc3493cfcd48a8efc8e6d01b1c6b0638bad
BRQ <bnowak4744@gmail.com>
2022-01-30 17:03:54 +0100

    Added solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    str_array = string.split("");
+
+    for(let i = 0; i < str_array.length/2; i++)
+    {
+        let temp = str_array[i];
+        str_array[i] = str_array[str_array.length-i-1];
+        str_array[str_array.length-i-1] = temp;
+    }
+    
+    string = str_array.join("");
 };
 
 // Do not edit below this line
