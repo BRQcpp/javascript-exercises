1e167699d8588925dbf47aa134148f9d64266e7c
BRQ <bnowak4744@gmail.com>
2022-02-13 13:17:51 +0100

    Added solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    let charArray = Array.from(word);
+    
+    charArray = charArray.filter( char =>
+    {
+        if(char.toUpperCase() == char.toLowerCase())
+            return false;
+        return true;
+    });
+        
+    charArray = charArray.map( char =>
+    {
+        return char.toLowerCase();
+    });
+        
+    for(let i = 0; i < charArray.length; i++)
+    {
+        if(charArray[i] != charArray[charArray.length-1-i])
+            return false;
+    }    
+    return true;
 };
 
 // Do not edit below this line
